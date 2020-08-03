0feb6a2156ff662654622949ce21f0ccb0940ca2
Kévin Rocha <neonix75@gmail.com>
2020-08-03 15:28:51 +0200

    Weather - modification to use async / await in my code

--- 2_Weather/app.js
+++ 2_Weather/app.js
@@ -6,38 +6,39 @@ const DOMweatherCity = document.querySelector(".weatherCity");
 
 const weather = {};
 
-let latitude = "48.8534";
-let longitude = "2.3488";
+main();
 
-getWeather(latitude, longitude);
+//http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=646d82915c35c27e56f30d7478d9c087
+
+//http://ip-api.com/json
 
 
 
-function getWeather(latitude, longitude) {
-    
-    const kelvin = 273;
-    const key = "646d82915c35c27e56f30d7478d9c087"
-    
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-            return data;
-        })
-        .then(function(data) {
-            weather.iconId = data.weather[0].icon;
-            weather.temp = Math.floor(data.main.temp) - kelvin;
-            weather.description = data.weather[0].description;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function(){
-            displayWeather();
-        });
+
+async function main () {
+    const city = await fetch(`http://ip-api.com/json`)
+        .then(result => result.json())
+        .then(json => json.city);
+
+    const weatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=646d82915c35c27e56f30d7478d9c087`)
+        .then(result => result.json())
+        .then(data => data);
+
+    displayWeather(weatherData);
 }
 
-function displayWeather() {
+
+
+
+function displayWeather(data) {
+    weather.iconId = data.weather[0].icon;
+    weather.temp = Math.floor(data.main.temp - 273);
+    weather.description = data.weather[0].description;
+    weather.city = data.name;
+    weather.country = data.sys.country;
+    
+    
     DOMweaterIcone.innerHTML = `<img src="icones/${weather.iconId}.png" alt="weather">`;
     DOMweatherTemp.innerHTML = `<p>${weather.temp}°<span class="weatherTempUnit"> C</span></p>`;
     DOMweatherDescription.innerHTML = `<p>${weather.description}</p>`;
