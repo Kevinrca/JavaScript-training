7998da743a3a3d7e28ba41105f4a9c33de041fb7
Kévin Rocha <neonix75@gmail.com>
2020-08-11 16:10:24 +0200

    Suggestion - JavaScript initialisation

---
+++ 5_Autosuggestion/app.js
@@ -0,0 +1,25 @@
+const citys = [
+    {name: "Paris"}, 
+    {name: "Lyon"}, 
+    {name: "Lille"}, 
+    {name: "Marseille"}, 
+    {name: "Berlin"}, 
+    {name: "Oslo"}, 
+    {name: "Nex York"}, 
+    {name: "Mehun sur Yèvre"}, 
+    {name: "Vierzon"}, 
+    {name: "Bourges"}
+];
+
+
+const cityInput = document.getElementById("cityInput");
+
+cityInput.addEventListener("input", () => searchCity(cityInput.value));
+
+
+
+
+function searchCity(searchText) {
+    
+}
+
