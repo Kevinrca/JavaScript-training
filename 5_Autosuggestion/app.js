403bca6fef0a6c375aecadd2128d16d1a510f051
Kévin Rocha <neonix75@gmail.com>
2020-08-11 16:52:07 +0200

    suggestion - implementation of the javascript code to suggeste properly citys when we type in

--- 5_Autosuggestion/app.js
+++ 5_Autosuggestion/app.js
@@ -1,3 +1,5 @@
+
+
 const citys = [
     {name: "Paris"}, 
     {name: "Lyon"}, 
@@ -8,11 +10,16 @@ const citys = [
     {name: "Nex York"}, 
     {name: "Mehun sur Yèvre"}, 
     {name: "Vierzon"}, 
-    {name: "Bourges"}
+    {name: "Bourges"},
+    {name: "Bourg"},
+    {name: "Bordeaux"},
+    {name: "Losanne"},
+    {name: "Listerine"}
 ];
 
-
+const suggestionsContainer = document.querySelector(".suggestions");
 const cityInput = document.getElementById("cityInput");
+
 
 cityInput.addEventListener("input", () => searchCity(cityInput.value));
 
@@ -20,6 +27,32 @@ cityInput.addEventListener("input", () => searchCity(cityInput.value));
 
 
 function searchCity(searchText) {
-    
-}
+    suggestionsContainer.innerHTML = "";
 
+    let matches = citys.filter(city => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return city.name.match(regex);
+    });
+
+    matches.forEach(element => {
+        suggestionsContainer.innerHTML += `<div class="suggestion">${element.name}</div>`;
+    });
+    
+
+
+    let suggestions = document.querySelectorAll(".suggestion");
+    suggestions.forEach(suggestion => {
+        suggestion.addEventListener("click", (e) => {
+            e.preventDefault();
+            cityInput.value = suggestion.innerHTML;
+            suggestionsContainer.innerHTML = "";
+        })
+    })
+
+
+
+
+    if(searchText.length === 0) {
+        suggestionsContainer.innerHTML = "";
+    }
+}
